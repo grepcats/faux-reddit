568c5691b6645de6bc491d55aa71c9c3da69cdc1
Kayla Ondracek <kayla.renee@gmail.com>
2018-03-30 08:41:46 -0700

    add fake post list, get lists of posts generated on home page via selector

--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -1,3 +1,4 @@
 export class Post {
-  //
+  public postedTo: string = "all";
+  constructor(public title: string, public postText: string, public upvotes: number, public downvotes: number){}
 }
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
+
+  @Input() postList: Post[];
 
   constructor() { }
 
