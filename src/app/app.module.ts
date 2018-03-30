0118a803683fe9247788180a980e0412eb33dab1
Kayla Ondracek <kayla.renee@gmail.com>
2018-03-30 08:54:03 -0700

    replace input structure with routing for post list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { PostListComponent } from './post-list/post-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
